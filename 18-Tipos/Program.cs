5f092b7c2d795a3f2ecd54cf03cfa6e3d5f3e2cd
Luana Yasmim Lourenço <lylourenco@sercom.com.br>
2022-07-28 16:11:27 -0300

    Tipos de referencia

---
+++ 18-Tipos/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+/*
+    Criar um ConsoleApplication que receba um valor inteiro na Main;
+    Criar um método void que receba esse inteiro e altere o seu valor para qualquer outro;
+    Exibir o valor alterado.
+*/
+
+
+static void Adicionar1(int x){
+    x++;
+}
+
+int a=2;
+Adicionar1(a);
+WriteLine(a);
+
+WriteLine("-----------------------------------------------------------------------------------");
